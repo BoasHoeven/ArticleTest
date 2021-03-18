d68a4f5d9d1f028afbbe16b0ab88dd0372e75b4d
Boas <BoasHoeven@gmail.com>
2021-03-18 15:33:44 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using JokesWebApp.Data;
+using JokesWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokesWebApp.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+		private readonly ApplicationDbContext _context;
+
+		public HomeController(ApplicationDbContext context, ILogger<HomeController> logger)
+		{
+			_logger = logger;
+			_context = context;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			return View(await _context.NewsArticle.ToListAsync());
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ Models/NewsArticle.cs
@@ -0,0 +1,13 @@
+﻿namespace JokesWebApp.Models
+{
+	public class NewsArticle
+	{
+		public int ID { get; set; }
+		public string Title { get; set; }
+		public string Content { get; set; }
+
+		public NewsArticle()
+		{
+		}
+	}
+}
